b5b1ab7605e4352b20639165c83f865eb1311c38
mohit270 <mohit21kundnani@gmail.com>
2023-09-14 21:01:10 +0530

    adding our app

---
+++ lib/recipeView.dart
@@ -0,0 +1,50 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class RecipeView extends StatefulWidget {
+  String url;
+  RecipeView(this.url);
+
+  @override
+  State<RecipeView> createState() => _RecipeViewState();
+}
+
+class _RecipeViewState extends State<RecipeView> {
+  late String finalUrl;
+  final Completer<WebViewController> controller =
+      Completer<WebViewController>();
+  @override
+  void initState() {
+    if (widget.url.toString().contains('http://')) {
+      finalUrl = widget.url.replaceAll('http://', 'https://');
+    } else {
+      finalUrl = widget.url;
+    }
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 22, 51, 65),
+          title: Text(
+            'Your Recipe ',
+          )),
+      body: Container(
+        child: WebView(
+          initialUrl: widget.url,
+          javascriptMode:
+              JavascriptMode.unrestricted, // Corrected the typo here
+          onWebViewCreated: (WebViewController webViewController) {
+            setState(() {
+              controller.complete(webViewController);
+            });
+          },
+        ),
+      ),
+    );
+  }
+}
